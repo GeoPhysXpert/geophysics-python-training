ca9c0f8bcdb1699b07b94c881bfcfc8e260bc1ca
GeoPhysXpert <astr0an21@gmail.com>
2026-03-11 11:26:11 +0530

    Calculate and print travel time based on distance and velocity

--- travel_time.py
+++ travel_time.py
@@ -1,1 +1,5 @@
-
+distance = 121
+velocity = 6
+travel_time = distance / velocity
+tt = round(travel_time, 2) #to round-up the travel-time to 2 decimal places
+print("travel time = ", tt, " seconds")
